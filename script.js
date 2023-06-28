2d46f4bed13d305084601835171fbec4af628275
Heitor Gonçalves <contato.heitor@icloud.com>
2023-06-27 22:15:16 -0300

    adiconado calculo de idade

--- script.js
+++ script.js
@@ -2,17 +2,31 @@
 // const month = document.getElementById("#month");
 // const year = doocument.getElementById("#year");
 
+const day = 27;
+const month = 1;
+const year = 1996;
+
 // const today = date.getDay();
 // const currentMonth = date.getMonth() + 1; 
 // const currentYear = date.getFullYear();
 
-// var dateNasc = new Date (day/month/year);
-// var dateFim = new Date(today/currentMonth/currentYear);
-// var timeDiff = Math.abs(dateFim.getTime() - dateNasc.getTime());
+// var date1 = new Date(´${day}/${month}/${year}´);
+// var date2 = new Date(´${today}/${currentMonth}/${currentYear}´);
+// var timeDiff = Math.abs(date2.getTime() - date1.getTime());
 // var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
 
-var date1 = new Date("7/11/2010");
-var date2 = new Date("12/12/2022");
-var timeDiff = Math.abs(date2.getTime() - date1.getTime());
-var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-alert(diffDays);
+// var diffDays = Math.floor((diffDays/365)); 
+var nascimento = `${month}/${day}/${year}`;
+var date1 = new Date(nascimento);
+
+var diff = Date.now() - date1;
+var idade = new Date(diff);
+
+var idadeAno = idade.getUTCFullYear() - 1970;
+var idadeMês = idade.getUTCMonth();
+var idadeDias = idade.getUTCDay();
+
+console.log(idadeAno, idadeMês, idadeDias);
+
+
+// alert(diffDays);
